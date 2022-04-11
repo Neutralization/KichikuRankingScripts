1317cad871df14c7f36ab9d65bb210bc25c3bbd9
Neutralization <50076677+Neutralization@users.noreply.github.com>
2022-04-11 18:11:12 +0800

    放弃mov

--- kichuku_旧稿回顾_新UI.jsx
+++ kichuku_旧稿回顾_新UI.jsx
@@ -10,7 +10,6 @@ CompFPS = 60;
 
 StaticFolder = app.project.items.addFolder('StaticResource');
 StaticResource = {
-    mask: './绿幕抠图/!旧稿回顾.mov',
     next: './绿幕抠图/!NEXT.mp4',
     change: './绿幕抠图/!NEXT 旧稿回顾.mp4',
 };
@@ -112,7 +111,8 @@ function AddProgressBar(Target, Length, Position, Duration, Offset, Delay) {
     NewLayer.startTime = Offset;
     NewLayer.outPoint = Duration + Offset;
     NewLayer.name = 'Progress';
-    VectorGroup = NewLayer.property('ADBE Root Vectors Group').addProperty('ADBE Vector Group').addProperty('ADBE Vectors Group');
+    VectorGroup = NewLayer.property('ADBE Root Vectors Group').addProperty('ADBE Vector Group')
+        .addProperty('ADBE Vectors Group');
     VectorGroup.addProperty('ADBE Vector Shape - Rect');
     VectorGroup.addProperty('ADBE Vector Graphic - Fill');
     VectorGroup.addProperty('ADBE Vector Graphic - Stroke');
@@ -265,7 +265,7 @@ for (rank = 1; rank <= LastRank; rank += 1) {
         ]);
     }
     RankVideoLayer.property('Position').setValue([960, 540]);
-    AddProgressBar(PreComp, CompSize[0], [960, 1074], SingleLength - 1, 0, 0);
+    AddProgressBar(PreComp, CompSize[0], [960, 1074], SingleLength - 0.5, 0, 1);
     t_fps = 2 * CompFPS;
     dest_y1 = -35;
     dest_y2 = 0;
@@ -288,14 +288,252 @@ for (rank = 1; rank <= LastRank; rank += 1) {
         ]);
     }
 
-    RankVideoMask = AddLayer(PreComp, 'mask', SingleLength, 0);
+    BottomRight = PreComp.layers.addShape();
+    BottomRight.name = '右下';
+    RightGroup = BottomRight.property('ADBE Root Vectors Group').addProperty('ADBE Vector Group');
+    RightShape = RightGroup.addProperty('ADBE Vectors Group');
+    RightShape.addProperty('ADBE Vector Shape - Rect');
+    RightShape.addProperty('ADBE Vector Graphic - Fill');
+    RightShape.addProperty('ADBE Vector Graphic - Stroke');
+    RightShape.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Size').setValue([321.3, 40]);
+    RightShape.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Roundness').setValue(20);
+    RightShape.property('ADBE Vector Graphic - Fill').property('ADBE Vector Fill Color').setValue([1, 1, 1, 1]);
+    RightShape.property('ADBE Vector Graphic - Stroke').property('ADBE Vector Stroke Width').setValue(0);
+    RightShape.property('ADBE Vector Graphic - Stroke').property('ADBE Vector Stroke Color').setValue([0, 0, 0, 1]);
+    BottomRight.property('Position').setValue([1306 + 793, 545 + 490]);
+
+    BottomLeft = PreComp.layers.addShape();
+    BottomLeft.name = '左下1';
+    LeftGroup = BottomLeft.property('ADBE Root Vectors Group').addProperty('ADBE Vector Group');
+    LeftShape = LeftGroup.addProperty('ADBE Vectors Group');
+    Shape1 = LeftShape.addProperty('ADBE Vector Shape - Rect');
+    Shape1.property('ADBE Vector Rect Size').setValue([220, 40]);
+    Shape1.property('ADBE Vector Rect Roundness').setValue(80);
+    Shape2 = LeftShape.addProperty('ADBE Vector Shape - Rect');
+    Shape2.property('ADBE Vector Rect Size').setValue([229, 40]);
+    Shape2.property('ADBE Vector Rect Roundness').setValue(80);
+    Shape2.property('ADBE Vector Rect Position').setValue([243, 0]);
+    LeftShape.addProperty('ADBE Vector Graphic - Fill');
+    LeftShape.property('ADBE Vector Graphic - Fill').property('ADBE Vector Fill Color').setValue([1, 1, 1, 1]);
+    LeftGroup.property('ADBE Vector Transform Group').property('ADBE Vector Anchor').setValue([-110, 0]);
+    BottomLeft.property('Position').setValue([107.2, 1036.9]);
+
+    BottomLeft2 = PreComp.layers.addShape();
+    BottomLeft2.name = '左下2';
+    LeftGroup2 = BottomLeft2.property('ADBE Root Vectors Group').addProperty('ADBE Vector Group');
+    LeftShape2 = LeftGroup2.addProperty('ADBE Vectors Group');
+    LeftShape2.addProperty('ADBE Vector Shape - Rect');
+    LeftShape2.addProperty('ADBE Vector Graphic - Fill');
+    LeftShape2.addProperty('ADBE Vector Filter - Repeater');
+    LeftShape2.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Size').setValue([285, 40]);
+    LeftShape2.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Roundness').setValue(80);
+    LeftShape2.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Position').setValue([517, 0]);
+    LeftShape2.property('ADBE Vector Graphic - Fill')
+        .property('ADBE Vector Fill Color').setValue([102 / 255, 102 / 255, 102 / 255, 1]);
+    LeftShape2.property('ADBE Vector Filter - Repeater').property('ADBE Vector Repeater Copies').setValue(2);
+    LeftShape2.property('ADBE Vector Filter - Repeater').property('ADBE Vector Repeater Transform')
+        .property('ADBE Vector Repeater Position').setValue([309, 0]);
+    LeftGroup2.property('ADBE Vector Transform Group').property('ADBE Vector Anchor').setValue([-110, 0]);
+    BottomLeft2.property('Position').setValue([107.2, 1036.9]);
+    BottomRamp = BottomLeft2.property('Effects').addProperty('ADBE Ramp');
+    BottomRamp.property('ADBE Ramp-0001').setValue([1000, 916]);
+    BottomRamp.property('ADBE Ramp-0002').setValue([36 / 255, 36 / 255, 36 / 255, 1]);
+    BottomRamp.property('ADBE Ramp-0003').setValue([1000, 1603]);
+    BottomRamp.property('ADBE Ramp-0004').setValue([1, 1, 1, 1]);
+    t_fps = 45;
+    dest_y1 = 0;
+    dest_y2 = 100;
+    dest = dest_y2 - dest_y1;
+    c1 = 34 / 45;
+    c2 = 0;
+    P1 = [0, 0];
+    P2 = [c1 * t_fps, 0];
+    P3 = [c2 * t_fps, dest];
+    P4 = [t_fps, dest];
+    for (x = 0; x <= t_fps; x += 1) {
+        y = BezierCurve(P1, P2, P3, P4, x);
+        BottomLeft.property('Scale').setValueAtTime((x + 38 + 100) / 60, [dest_y1 + y, dest_y1 + y]);
+        BottomLeft2.property('Scale').setValueAtTime((x + 38 + 100) / 60, [dest_y1 + y, dest_y1 + y]);
+    }
+    t_fps = 45;
+    dest_y1 = 100;
+    dest_y2 = 0;
+    dest = dest_y2 - dest_y1;
+    c1 = 1;
+    c2 = 11 / 45;
+    P1 = [0, 0];
+    P2 = [c1 * t_fps, 0];
+    P3 = [c2 * t_fps, dest];
+    P4 = [t_fps, dest];
+    for (x = 0; x <= t_fps; x += 1) {
+        y = BezierCurve(P1, P2, P3, P4, x);
+        BottomLeft.property('Scale').setValueAtTime((x + 1430) / 60, [dest_y1 + y, dest_y1 + y]);
+        BottomLeft2.property('Scale').setValueAtTime((x + 1430) / 60, [dest_y1 + y, dest_y1 + y]);
+    }
+
+    TopLeft = PreComp.layers.addShape();
+    TopLeft.name = '左上';
+    TopGroup = TopLeft.property('ADBE Root Vectors Group').addProperty('ADBE Vector Group');
+    TopShape = TopGroup.addProperty('ADBE Vectors Group');
+    TopShape.addProperty('ADBE Vector Shape - Rect');
+    TopShape.addProperty('ADBE Vector Graphic - Fill');
+    TopShape.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Size').setValue([279, 70]);
+    t_fps = 83;
+    dest_x1 = 0;
+    dest_x2 = 279;
+    dest_x = dest_x2 - dest_x1;
+    dest_y1 = 90;
+    dest_y2 = 70;
+    dest_y = dest_y2 - dest_y1;
+    c1 = 66 / 83;
+    c2 = 56 / 83;
+    P1 = [0, 0];
+    P2 = [c1 * t_fps, 0];
+    P3 = [c2 * t_fps, dest_x];
+    P4 = [t_fps, dest_x];
+    P5 = [c2 * t_fps, dest_y];
+    P6 = [t_fps, dest_y];
+    for (t = 0; t <= t_fps; t += 1) {
+        x = BezierCurve(P1, P2, P3, P4, t);
+        y = BezierCurve(P1, P2, P5, P6, t);
+        TopShape.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Size')
+            .setValueAtTime((t + 27 + 100) / 60, [dest_x1 + x, dest_y1 + y]);
+    }
+    t_fps = 45;
+    dest_x1 = 279;
+    dest_x2 = 0;
+    dest_x = dest_x2 - dest_x1;
+    dest_y1 = 70;
+    dest_y2 = 90;
+    dest_y = dest_y2 - dest_y1;
+    c1 = 35 / 45;
+    c2 = 9 / 45;
+    P1 = [0, 0];
+    P2 = [c1 * t_fps, 0];
+    P3 = [c2 * t_fps, dest_x];
+    P4 = [t_fps, dest_x];
+    P5 = [c2 * t_fps, dest_y];
+    P6 = [t_fps, dest_y];
+    for (t = 0; t <= t_fps; t += 1) {
+        x = BezierCurve(P1, P2, P3, P4, t);
+        y = BezierCurve(P1, P2, P5, P6, t);
+        TopShape.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Size')
+            .setValueAtTime((t + 1422) / 60, [dest_x1 + x, dest_y1 + y]);
+    }
+    TopShape.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Roundness').setValue(20);
+    // TopShape.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Position').setValue([-78, -177]);
+    TopShape.property('ADBE Vector Graphic - Fill').property('ADBE Vector Fill Color').setValue([1, 1, 1, 1]);
+    // TopGroup.property('ADBE Vector Transform Group').property('ADBE Vector Anchor').setValue([-110, 0]);
+    TopLeft.property('Position').setValue([246, 95]);
+    TopRamp = TopLeft.property('Effects').addProperty('ADBE Ramp');
+    TopRamp.property('ADBE Ramp-0001').setValue([960, 0]);
+    TopRamp.property('ADBE Ramp-0002').setValue([36 / 255, 36 / 255, 36 / 255, 1]);
+    TopRamp.property('ADBE Ramp-0003').setValue([960, 699]);
+    TopRamp.property('ADBE Ramp-0004').setValue([1, 1, 1, 1]);
+
+    TopRight = PreComp.layers.addShape();
+    TopRight.name = '右上';
+    TopRightGroup = TopRight.property('ADBE Root Vectors Group').addProperty('ADBE Vector Group');
+    TopRightShape = TopRightGroup.addProperty('ADBE Vectors Group');
+    TopRightShape.addProperty('ADBE Vector Shape - Rect');
+    TopRightShape.addProperty('ADBE Vector Graphic - Fill');
+    TopRightShape.addProperty('ADBE Vector Graphic - Stroke');
+    TopRightShape.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Size').setValue([1436, 70]);
+    TopRightShape.property('ADBE Vector Shape - Rect').property('ADBE Vector Rect Roundness').setValue(20);
+    TopRightShape.property('ADBE Vector Graphic - Fill').property('ADBE Vector Fill Color').setValue([1, 1, 1, 1]);
+    TopRightShape.property('ADBE Vector Graphic - Stroke').property('ADBE Vector Stroke Width').setValue(0);
+    TopRightShape.property('ADBE Vector Graphic - Stroke').property('ADBE Vector Stroke Color').setValue([0, 0, 0, 1]);
+    TopRight.property('Position').setValue([1184, 95]);
+    t_fps = 78;
+    dest_y1 = 1457;
+    dest_y2 = 1184;
+    dest = dest_y2 - dest_y1;
+    c1 = 70 / 78;
+    c2 = 0;
+    P1 = [0, 0];
+    P2 = [c1 * t_fps, 0];
+    P3 = [c2 * t_fps, dest];
+    P4 = [t_fps, dest];
+    for (x = 0; x <= t_fps; x += 1) {
+        y = BezierCurve(P1, P2, P3, P4, x);
+        TopRight.property('Position').setValueAtTime((x + 100) / 60, [dest_y1 + y, 95]);
+    }
+    t_fps = 66;
+    dest_y1 = 1184;
+    dest_y2 = 1457;
+    dest = dest_y2 - dest_y1;
+    c1 = 1;
+    c2 = 7 / 78;
+    P1 = [0, 0];
+    P2 = [c1 * t_fps, 0];
+    P3 = [c2 * t_fps, dest];
+    P4 = [t_fps, dest];
+    for (x = 0; x <= t_fps; x += 1) {
+        y = BezierCurve(P1, P2, P3, P4, x);
+        TopRight.property('Position').setValueAtTime((x + 1400) / 60, [dest_y1 + y, 95]);
+    }
+    TopRight.property('Opacity').setValueAtTime(0 + 2, 0);
+    TopRight.property('Opacity').setValueAtTime(1.5 + 2, 100);
+    TopRight.property('Opacity').setValueAtTime(SingleLength - 5 / 3, 100);
+    TopRight.property('Opacity').setValueAtTime(SingleLength - 0.5, 0);
+
+    TLayer = PreComp.layers.addText('旧稿回顾');
+    TDocument = TLayer.property('Source Text').value;
+    TDocument.resetCharStyle();
+    TDocument.resetParagraphStyle();
+    TDocument.applyFill = false;
+    TDocument.applyStroke = true;
+    TDocument.justification = ParagraphJustification.LEFT_JUSTIFY;
+    TLayer.property('Source Text').setValue(TDocument);
+    TLayer.property('Source Text').expression =
+        'text.sourceText.createStyle().setFont("HarmonyOS_Sans_SC_Bold")' +
+        '.setFontSize(76).setLeading(0).setStrokeWidth(1).setStrokeColor(hexToRgb("FFFFFF")).setFauxItalic(true);';
+    TLayer.property('Source Text').expression.enabled = true;
+    TLayer.property('Position').setValue([2091 - 152, 988]);
+    t_fps = 66;
+    dest_y1 = 2091 - 152;
+    dest_y2 = 1724 - 152;
+    dest = dest_y2 - dest_y1;
+    c1 = 1 / 3;
+    c2 = 0;
+    P1 = [0, 0];
+    P2 = [c1 * t_fps, 0];
+    P3 = [c2 * t_fps, dest];
+    P4 = [t_fps, dest];
+    for (x = 0; x <= t_fps; x += 1) {
+        y = BezierCurve(P1, P2, P3, P4, x);
+        TLayer.property('Position').setValueAtTime((x + 11 + 100) / 60, [dest_y1 + y, 988]);
+    }
+    t_fps = 66;
+    dest_y1 = 1724 - 152;
+    dest_y2 = 2213 - 152;
+    dest = dest_y2 - dest_y1;
+    c1 = 1;
+    c2 = 30 / 66;
+    P1 = [0, 0];
+    P2 = [c1 * t_fps, 0];
+    P3 = [c2 * t_fps, dest];
+    P4 = [t_fps, dest];
+    for (x = 0; x <= t_fps; x += 1) {
+        y = BezierCurve(P1, P2, P3, P4, x);
+        TLayer.property('Position').setValueAtTime((x + 1400) / 60, [dest_y1 + y, 988]);
+    }
+    BottomRight.parent = TLayer;
+    Shadow = TLayer.property('Effects').addProperty('ADBE Drop Shadow');
+    Shadow.property('ADBE Drop Shadow-0001').setValue([0, 0, 0]);
+    Shadow.property('ADBE Drop Shadow-0002').setValue(255);
+    Shadow.property('ADBE Drop Shadow-0003').setValue(120);
+    Shadow.property('ADBE Drop Shadow-0004').setValue(5);
+    Shadow.property('ADBE Drop Shadow-0005').setValue(7);
+
     RankDataLayer = AddLayer(PreComp, rank + '_T', SingleLength, 0);
-    RankDataLayer.property('Opacity').setValueAtTime(1, 0);
-    RankDataLayer.property('Opacity').setValueAtTime(1.5, 100);
+    RankDataLayer.property('Opacity').setValueAtTime(1 + 2, 0);
+    RankDataLayer.property('Opacity').setValueAtTime(1.5 + 2, 100);
     RankDataLayer.property('Opacity').setValueAtTime(SingleLength - 1.5, 100);
     RankDataLayer.property('Opacity').setValueAtTime(SingleLength - 1, 0);
     PreRankLayer = AddLayer(Part1, 'Pre' + rank + '_V', SingleLength, Globaloffset);
     Shadow = PreRankLayer.property('Effects').addProperty('ADBE Drop Shadow');
+    Shadow.property('ADBE Drop Shadow-0001').setValue([0, 0, 0]);
     Shadow.property('ADBE Drop Shadow-0002').setValue(100);
     Shadow.property('ADBE Drop Shadow-0004').setValue(0);
     Shadow.property('ADBE Drop Shadow-0005').setValue(50);
@@ -340,7 +578,8 @@ for (rank = 1; rank <= LastRank; rank += 1) {
     P4 = [t_fps, 0];
     for (x = 0; x <= t_fps; x += 1) {
         y = BezierCurve(P1, P2, P3, P4, x);
-        PreRankLayer.property('Scale').setValueAtTime(Globaloffset + SingleLength - 1 + x / 60, [dest_y1 + y, dest_y1 + y]);
+        PreRankLayer.property('Scale')
+            .setValueAtTime(Globaloffset + SingleLength - 1 + x / 60, [dest_y1 + y, dest_y1 + y]);
     }
     t_fps = CompFPS / 2;
     dest_y1 = 540;
@@ -354,7 +593,8 @@ for (rank = 1; rank <= LastRank; rank += 1) {
     P4 = [t_fps, dest];
     for (x = 0; x <= t_fps; x += 1) {
         y = BezierCurve(P1, P2, P3, P4, x);
-        PreRankLayer.property('Position').setValueAtTime(Globaloffset + SingleLength - 0.5 + (x - 1) / 60, [960, dest_y1 + y]);
+        PreRankLayer.property('Position')
+            .setValueAtTime(Globaloffset + SingleLength - 0.5 + (x - 1) / 60, [960, dest_y1 + y]);
     }
 
     if (rank == LastRank) {
