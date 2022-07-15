d8ebf9c757cb21134e43fd8867eaeaa484091074
Neutralization <50076677+Neutralization@users.noreply.github.com>
2022-07-15 15:23:04 +0800

    变量名调整&优化

--- rankorder.py
+++ rankorder.py
@@ -12,11 +12,11 @@ from pandas import Series, read_csv, read_excel, to_datetime
 
 PAST = arrow.get("2021-11-08 00:00:00", "YYYY-MM-DD HH:mm:ss")
 NOW = arrow.now()
-weeks = int((NOW.timestamp() - PAST.timestamp()) / 3600 / 24 / 7) + 1
-weekday = int(NOW.format("d"))
-last_date = NOW.shift(days=-(weekday + 13)).format("YYYY-MM-DD")
-this_date = NOW.shift(days=-(weekday + 6)).format("YYYY-MM-DD")
-end_date = NOW.shift(days=-(weekday - 1)).format("YYYY-MM-DD")
+WEEKS = int((NOW.timestamp() - PAST.timestamp()) / 3600 / 24 / 7) + 1
+WEEKDAY = int(NOW.format("d"))
+BEFORE = NOW.shift(days=-(WEEKDAY + 13)).format("YYYY-MM-DD")
+LAST = NOW.shift(days=-(WEEKDAY + 6)).format("YYYY-MM-DD")
+THIS = NOW.shift(days=-(WEEKDAY - 1)).format("YYYY-MM-DD")
 
 
 async def getusername(uid):
@@ -72,18 +72,17 @@ def downcover(rank, aid, link):
 
 
 def readExcel(filename):
+    global excluded
     print(f"\n加载文件\n\t{abspath(filename)}")
     df = read_csv(filename)
     df.sort_values(by="总分", inplace=True, ascending=False)
     df = df.reset_index(drop=True)
-    print(f"\n加载文件\n\t{abspath('周刊除外.csv')}")
-    ex_aids = [int(line.strip("\n")) for line in open("周刊除外.csv", "r")]
-    for aid in ex_aids:
-        exclude = df.loc[df["aid"] == aid].index
-        df = df.drop(exclude)
+    for aid in excluded:
+        excluded = df.loc[df["aid"] == aid].index
+        df = df.drop(excluded)
         df = df.sort_index().reset_index(drop=True)
     columns = df.columns.to_list()
-    df.insert(columns.index("mid") + 1, "up主", [i + 1 for i in range(len(df.index))])
+    df.insert(columns.index("mid") + 1, "up主", [""] * len(df.index))
     df.insert(0, "排名", [i + 1 for i in range(len(df.index))])
     df.insert(0, "评语", [""] * len(df.index))
 
@@ -97,31 +96,34 @@ def readExcel(filename):
 
 
 def diffExcel(file1, file2):
-    df1 = readExcel(file1)
-    df2 = readExcel(file2)
+    new = readExcel(file1)
+    old = readExcel(file2)
     print(f"\n加载文件\n\t{abspath('周刊长期.xlsx')}")
-    df3 = read_excel("周刊长期.xlsx")
+    long = read_excel("周刊长期.xlsx")
     long_array = []
-    for i in range(len(df1.index)):
-        # 新稿件
-        if df2.loc[df2["aid"] == df1.at[i, "aid"]].empty:
-            df1.at[i, "评语"] = "New"
+    for i in range(len(new.index)):
+        # 本周出现的新稿件
+        if old.loc[old["aid"] == new.at[i, "aid"]].empty:
+            new.at[i, "评语"] = "New"
             continue
-        # 旧稿件
-        lastrank = df2.loc[df2["aid"] == df1.at[i, "aid"]]["排名"].array[0]
-        # 副榜外
+        lastrank = old.loc[old["aid"] == new.at[i, "aid"]]["排名"].array[0]
+        # 上周排名在副榜外记作新上榜
         if lastrank > 125:
-            df1.at[i, "评语"] = "New"
+            new.at[i, "评语"] = "New"
             continue
-        # 主榜三期连续在榜
-        if df1.at[i, "排名"] <= 20 + len(long_array):
+        # 六周内
+        # 连续进入主榜三次记作连续在榜
+        # 本周必须严格进入主榜
+        # 进入连续在榜后
+        # 连续掉出主榜三次退出连续在榜
+        if new.at[i, "排名"] <= 20 + len(long_array):
             long_status = [
-                bool(not df3.loc[df3[weeks - 5] == df1.at[i, "aid"]].empty),
-                bool(not df3.loc[df3[weeks - 4] == df1.at[i, "aid"]].empty),
-                bool(not df3.loc[df3[weeks - 3] == df1.at[i, "aid"]].empty),
-                bool(not df3.loc[df3[weeks - 2] == df1.at[i, "aid"]].empty),
-                bool(not df3.loc[df3[weeks - 1] == df1.at[i, "aid"]].empty),
-                bool(df1.at[i, "排名"] <= 20),
+                bool(not long.loc[long[WEEKS - 5] == new.at[i, "aid"]].empty),
+                bool(not long.loc[long[WEEKS - 4] == new.at[i, "aid"]].empty),
+                bool(not long.loc[long[WEEKS - 3] == new.at[i, "aid"]].empty),
+                bool(not long.loc[long[WEEKS - 2] == new.at[i, "aid"]].empty),
+                bool(not long.loc[long[WEEKS - 1] == new.at[i, "aid"]].empty),
+                bool(new.at[i, "排名"] <= 20),
             ]
             if long_status[3:6] == [True] * 3:
                 long_array.append(i)
@@ -133,41 +135,34 @@ def diffExcel(file1, file2):
                 long_array.append(i)
             else:
                 pass
-        df1.at[i, "评语"] = f"上周{lastrank}"
+        new.at[i, "评语"] = f"上周{lastrank}"
 
     print("\n获取UP主昵称...")
     nametasks = [
-        asyncio.ensure_future(getusername(int(df1.at[x, "mid"])))
-        for x in df1[0:150].index
+        asyncio.ensure_future(getusername(int(new.at[x, "mid"])))
+        for x in new[0 : 20 + len(long_array)].index
     ]
     nameloop = asyncio.get_event_loop()
     usernames = nameloop.run_until_complete(asyncio.gather(*nametasks))
     usernames = reduce(lambda x, y: {**x, **y}, usernames)
-    for x in df1[0:150].index:
-        df1.at[x, "up主"] = usernames[df1.at[x, "mid"]]
+    for x in new[0 : 20 + len(long_array)].index:
+        new.at[x, "up主"] = usernames[new.at[x, "mid"]]
 
-    onrank = df1.loc[df1["排名"] <= 20 + len(long_array)]["aid"].to_list()
-    df3[weeks] = Series(onrank)
-    df3.to_excel("周刊长期.xlsx", index=False)
+    onrank = new.loc[new["排名"] <= 20 + len(long_array)]["aid"].to_list()
+    long[WEEKS] = Series(onrank)
+    long.to_excel("周刊长期.xlsx", index=False)
 
     if len(long_array) > 0:
-        long = df1.iloc[long_array, :]
+        long = new.iloc[long_array, :]
         onarray = long.loc[long["排名"] > 20 + len(long_array)].index
         long = long.drop(onarray)
-        long.to_excel(f"{weeks:03d}期连续在榜.xlsx", index=False)
-        df1.drop(long_array, inplace=True)
-    df1 = df1.sort_index().reset_index(drop=True)
-
-    df1.loc[2.5] = df1.columns.to_list()
-    df1.loc[9.5] = df1.columns.to_list()
-    df1.loc[19.5] = df1.columns.to_list()
-    df1 = df1.sort_index().reset_index(drop=True)
+        long.to_excel(f"{WEEKS:03d}期连续在榜.xlsx", index=False)
+        new.drop(long_array, inplace=True)
+    new = new.sort_index().reset_index(drop=True)
 
     print("\n获取视频封面...")
     covertasks = [
-        asyncio.ensure_future(getcover(int(df1.at[x, "aid"])))
-        for x in df1[0:128].index
-        if df1.at[x, "aid"] != "aid"
+        asyncio.ensure_future(getcover(int(new.at[x, "aid"]))) for x in new[0:125].index
     ]
     coverloop = asyncio.get_event_loop()
     covers = coverloop.run_until_complete(asyncio.gather(*covertasks))
@@ -175,23 +170,20 @@ def diffExcel(file1, file2):
     list(
         map(
             downcover,
-            [int(df1.at[x, "排名"]) for x in df1[0:128].index if df1.at[x, "排名"] != "排名"],
-            [
-                int(df1.at[x, "aid"])
-                for x in df1[0:128].index
-                if df1.at[x, "aid"] != "aid"
-            ],
-            [
-                covers[int(df1.at[x, "aid"])]
-                for x in df1[0:128].index
-                if df1.at[x, "aid"] != "aid"
-            ],
+            [int(new.at[x, "排名"]) for x in new[0:125].index],
+            [int(new.at[x, "aid"]) for x in new[0:125].index],
+            [covers[int(new.at[x, "aid"])] for x in new[0:125].index],
         )
     )
 
-    df1[0:128].to_excel(f"{weeks:03d}期主榜.xlsx", index=False)
+    new.loc[2.5] = new.columns.to_list()
+    new.loc[9.5] = new.columns.to_list()
+    new.loc[19.5] = new.columns.to_list()
+    new = new.sort_index().reset_index(drop=True)
+    new[0:128].to_excel(f"{WEEKS:03d}期主榜.xlsx", index=False)
+
     print(
-        df1.loc[
+        new.loc[
             :21,
             ["排名", "aid", "bvid", "up主", "title"],
         ]
@@ -199,9 +191,12 @@ def diffExcel(file1, file2):
 
 
 def main():
-    this_excel = f"./统计数据/{this_date}_to_{end_date}.csv"
-    past_excel = f"./统计数据/{last_date}_to_{this_date}.csv"
-    diffExcel(this_excel, past_excel)
+    global excluded
+    print(f"\n加载文件\n\t{abspath('周刊除外.csv')}")
+    excluded = [int(line.strip("\n")) for line in open("周刊除外.csv", "r")]
+    THIS_WEEK = f"./统计数据/{LAST}_to_{THIS}.csv"
+    LAST_WEEK = f"./统计数据/{BEFORE}_to_{LAST}.csv"
+    diffExcel(THIS_WEEK, LAST_WEEK)
     input("\n执行完毕，可以退出\n")
 
 
