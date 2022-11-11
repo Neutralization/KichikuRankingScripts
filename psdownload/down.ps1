ec371e4f05aaf18c92a382aea891fd00f5863fc4
Neutralization <50076677+Neutralization@users.noreply.github.com>
2022-11-11 19:12:10 +0800

    重写

--- psdownload/down.ps1
+++ psdownload/down.ps1
@@ -1,90 +1,154 @@
-﻿$ProgressPreference = 'SilentlyContinue'
+﻿$ProgressPreference = "SilentlyContinue"
+$TruePath = Split-Path $MyInvocation.MyCommand.Path
+$CookieFile = "$($TruePath)/cookies.txt"
+$DownloadList = "$($TruePath)/download.txt"
+$DownloadFolder = "$($TruePath)/../主榜视频"
+$UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26"
+
+$Session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$Session.UserAgent = $UA
+$CookieString = ""
+if (Test-Path $CookieFile) {
+    $Cookies = Get-Content -Path $CookieFile
+    $Cookies | ForEach-Object {
+        if (!$_.StartsWith("#") -and $_.StartsWith(".bilibili.com")) {
+            $Cookie = $_.Split("`t")
+            $Name = $Cookie[5]
+            $Value = $Cookie[6]
+            $Path = $Cookie[2]
+            $Domain = $Cookie[0]
+            $CookieString += "$($Name)=$($Value); "
+            $Session.Cookies.Add((New-Object System.Net.Cookie($Name, $Value, $Path, $Domain)))
+        }
+    }
+}
+$Headers = @{
+    "authority"  = "api.bilibili.com"
+    "method"     = "GET"
+    "path"       = ""
+    "scheme"     = "https"
+    "origin"     = "https://www.bilibili.com"
+    "referer"    = ""
+    "user-agent" = $UA
+}
+if ("" -ne $CookieString) { $Headers.cookie = $CookieString }
+
+function ABconvert {
+    param (
+        [parameter(position = 1)]$Source,
+        [parameter(position = 2)]$Target = $true
+    )
+    # 如何看待 2020 年 3 月 23 日哔哩哔哩将稿件的「av 号」变更为「BV 号」？ - mcfx的回答 - 知乎
+    # https://www.zhihu.com/question/381784377/answer/1099438784
+    $table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF".ToCharArray()
+    $tr = @{}
+    0..57 | ForEach-Object {
+        $tr[$table[$_]] = $_
+    }
+    $s = @(11, 10, 3, 8, 4, 6)
+    $xor = 177451812
+    $add = 8728348608
+
+    function dec {
+        param (
+            [string]$x
+        )
+        $r = 0
+        0..5 | ForEach-Object {
+            $r += $tr[$x[$s[$_]]] * [Math]::Pow(58, $_)
+        }
+        return (($r - $add) -bxor $xor)
+    }
+    function enc {
+        param (
+            [string]$x
+        )
+        $x = ($x -bxor $xor) + $add
+        $r = "BV1  4 1 7  ".ToCharArray()
+        0..5 | ForEach-Object {
+            $r[$s[$_]] = $table[[Math]::Floor($x / [Math]::Pow(58, $_)) % 58]
+        }
+        return -join $r
+    }
+    if ($Target) {
+        return dec $Source
+    }
+    else {
+        return enc $Source
+    }
+}
 
 function BiliDown {
     param (
-        [parameter(position = 1)]$BID,
-        [parameter(position = 2)]$Part = ''
+        [parameter(position = 1)]$ID,
+        [parameter(position = 2)]$Part = 1
     )
-    $Headers = @{}
-    $Headers.Add('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0')
-    $Headers.Add('Referer', 'https://www.bilibili.com')
 
-    if ($BID.StartsWith('a') -or $BID.StartsWith('A')) {
-        $VID = $BID.Substring(2)
-        $PageList = "https://api.bilibili.com/x/player/pagelist?aid=$($VID)&jsonp=jsonp"
-        $SourcePrefix = 'https://api.bilibili.com/x/player/playurl?avid'
-    } elseif ($BID.StartsWith('b') -or $BID.StartsWith('B')) {
-        $VID = $BID
-        $PageList = "https://api.bilibili.com/x/player/pagelist?bvid=$($VID)&jsonp=jsonp"
-        $SourcePrefix = 'https://api.bilibili.com/x/player/playurl?bvid'
-    } else {
+    if ($ID -match "^[aA]") {
+        $AID = $ID.Substring(2)
+        $BID = ABconvert $ID.Substring(2) $false
+    }
+    elseif ($ID -match "^[bB]") {
+        $AID = ABconvert $ID $true
+        $BID = $ID
+    }
+    else {
         exit
     }
-    $Pages = (Invoke-WebRequest -Uri $PageList -Headers $Headers -UseBasicParsing).Content | ConvertFrom-Json
-    $CIDs = @()
-    if ($Part -EQ '') {
-        $CIDs += $Pages.data | Where-Object page -EQ 1 | Select-Object cid
-    } elseif ($Part -EQ '0') {
-        $CIDs += $Pages.data | Select-Object cid
-    } else {
-        $CIDs += $Pages.data | Where-Object page -EQ $Part | Select-Object cid
-    }
-    Write-Host "$($BID) Video Downloading......"
-    $CIDs | ForEach-Object {
-        function DownWithFFMPEG {
-            param (
-                [parameter(position = 1)]$CID,
-                [parameter(position = 2)]$CIDIndex
-            )
-            $NormalUrl = "https://www.bilibili.com/video/$($BID)"
-            $ReDirectTest = Invoke-WebRequest -Method Head -Uri $NormalUrl -Headers $Headers -UseBasicParsing
-            if ($null -ne $ReDirectTest.RequestMessage.RequestUri -and $ReDirectTest.RequestMessage.RequestUri.ToString() -Match 'bangumi' ) {
-                # Write-Host $ReDirectTest.BaseResponse.RequestMessage.RequestUri
-                $EPId = $ReDirectTest.BaseResponse.RequestMessage.RequestUri.ToString().Split('/')[-1].Substring(2)
-                $PGCSourceUrl = "https://api.bilibili.com/pgc/player/web/playurl?cid=$($CID)&qn=120&type=&otype=json&fourk=1&ep_id=$($EPId)&fnver=0&fnval=80"
-                $VideoData = (Invoke-WebRequest -Uri $PGCSourceUrl -Headers $Headers -UseBasicParsing).Content | ConvertFrom-Json
-                $SourceFiles = $VideoData.result.dash
-            } else {
-                $VideoData = (Invoke-WebRequest -Uri $SourceUrl -Headers $Headers -UseBasicParsing).Content | ConvertFrom-Json
-                $SourceFiles = $VideoData.data.dash
-            }
-            try {
-                $aria2cArgs = "-x16 -s12 -j20 -k1M --continue --check-certificate=false --file-allocation=none --summary-interval=0 --download-result=hide --disable-ipv6 ""$($SourceFiles.audio[0].baseUrl)"" --header=""User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"" --header=""Referer: https://www.bilibili.com"" --out ../主榜视频/$($CID)_a.m4s"
-                Start-Process -NoNewWindow -Wait -FilePath 'aria2c.exe' -RedirectStandardError "../主榜视频/$($CID)_.log" -ArgumentList $aria2cArgs
+    # Write-Output $AID
+    # Write-Output $BID
 
-                $aria2cArgs = "-x16 -s12 -j20 -k1M --continue --check-certificate=false --file-allocation=none --summary-interval=0 --download-result=hide --disable-ipv6 ""$($SourceFiles.video[0].baseUrl)"" --header=""User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"" --header=""Referer: https://www.bilibili.com"" --out ../主榜视频/$($CID)_v.m4s"
-                Start-Process -NoNewWindow -Wait -FilePath 'aria2c.exe' -RedirectStandardError "../主榜视频/$($CID)_.log" -ArgumentList $aria2cArgs
-                if ($CIDIndex -EQ '0') {
-                    $Filename = $BID
-                } else {
-                    $Filename = "$($BID)_$($CIDIndex)"
-                }
-                $ffmpegArgs = "-y -hide_banner -i ../主榜视频/$($CID)_a.m4s -i ../主榜视频/$($CID)_v.m4s -c copy ../主榜视频/$($Filename).mp4"
-                Start-Process -NoNewWindow -Wait -FilePath 'ffmpeg.exe' -RedirectStandardError "../主榜视频/$($CID)_.log" -ArgumentList $ffmpegArgs
-                Remove-Item "../主榜视频/$($CID)_*.*"
-            } catch {
-                New-Item -Path '../主榜视频/' -Name "$($BID).txt" -ItemType 'file' -Value '' -Force
-            }
-        }
-        $OID = $_
-        $SourceUrl = "$($SourcePrefix)=$($VID)&cid=$($OID.cid)&qn=120&fnver=0&fourk=1&fnval=80&otype=json&type="
-        DownWithFFMPEG $OID.cid $CIDs.IndexOf($OID)
+    $PageList = "https://api.bilibili.com/x/player/pagelist?aid=$($AID)&jsonp=jsonp"
+    $Headers.referer = "https://www.bilibili.com/video/av$($AID)/"
+    $Headers.path = "/x/player/pagelist?aid=$($AID)&jsonp=jsonp"
+    $Pages = Invoke-WebRequest -UseBasicParsing -Uri $PageList -WebSession $Session -Headers $Headers |
+    Select-Object -ExpandProperty "Content" | ConvertFrom-Json
+    $CID = $Pages.data | Where-Object -Property "page" -EQ $Part | Select-Object -ExpandProperty "cid"
+    # Write-Output $CID
+
+    $SourceUrl = "https://api.bilibili.com/x/player/playurl?avid=$($AID)&bvid=$($BID)&cid=$($CID)&qn=120&fnver=0&fnval=4048&fourk=1&voice_balance=1"
+    # Write-Output $SourceUrl
+    $VideoData = Invoke-WebRequest -UseBasicParsing -Uri $SourceUrl -WebSession $Session -Headers $Headers |
+    Select-Object -ExpandProperty "Content" | ConvertFrom-Json
+    Write-Host "$($ID) Video Downloading......"
+
+    $AudioID = $VideoData.data.dash.audio.id | Measure-Object -Maximum | Select-Object -ExpandProperty "Maximum"
+    $AudioDASH = $VideoData.data.dash.audio | Where-Object -Property "id" -EQ $AudioID |
+    Select-Object -ExpandProperty "baseUrl"
+    $VideoID = $VideoData.data.dash.video.id | Measure-Object -Maximum | Select-Object -ExpandProperty "Maximum"
+    $VideoDASH = $VideoData.data.dash.video | Where-Object -Property "id" -EQ $VideoID |
+    Where-Object -Property "codecs" -match "avc" | Select-Object -ExpandProperty "baseUrl"
+    # Write-Output $AudioDASH
+    # Write-Output $VideoDASH
+
+    try {
+        $aria2cArgs = "-x16 -s12 -j20 -k1M --continue --check-certificate=false --file-allocation=none --summary-interval=0 --download-result=hide ""$($AudioDASH)"" --header=""$($UA)"" --header=""Referer: https://www.bilibili.com"" --dir=$($DownloadFolder) --out $($CID)_a.m4s"
+        Start-Process -NoNewWindow -Wait -FilePath "aria2c.exe" -ArgumentList $aria2cArgs -RedirectStandardError "$($DownloadFolder)/$($CID)_.log"
+
+        $aria2cArgs = "-x16 -s12 -j20 -k1M --continue --check-certificate=false --file-allocation=none --summary-interval=0 --download-result=hide ""$($VideoDASH)"" --header=""User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"" --header=""Referer: https://www.bilibili.com"" --dir=$($DownloadFolder) --out $($CID)_v.m4s"
+        Start-Process -NoNewWindow -Wait -FilePath "aria2c.exe" -ArgumentList $aria2cArgs -RedirectStandardError "$($DownloadFolder)/$($CID)_.log"
+
+        $ffmpegArgs = "-y -hide_banner -i $($DownloadFolder)/$($CID)_a.m4s -i $($DownloadFolder)/$($CID)_v.m4s -c copy $($DownloadFolder)/$($ID).mp4"
+        Start-Process -NoNewWindow -Wait -FilePath "ffmpeg.exe" -ArgumentList $ffmpegArgs -RedirectStandardError "$($DownloadFolder)/$($CID)_.log"
     }
+    catch { New-Item -Path "$($DownloadFolder)" -Name "$($BID).txt" -ItemType "file" -Value "" -Force }
+    Remove-Item "$($DownloadFolder)/$($CID)_*.*"
 }
 
 function Main {
     $RankVideos = @()
-
-    Get-Content './download.txt' | ForEach-Object {
-        $RankVideos += "$($_)"
-    }
+    Get-Content -Path $DownloadList | ForEach-Object { $RankVideos += "$($_)" }
 
     $ExistVideos = @()
-    Get-Item '../主榜视频/*.mp4' | ForEach-Object { $ExistVideos += $_.BaseName }
+    if (!(Test-Path $DownloadFolder)) {
+        New-Item -Path $DownloadFolder -ItemType "directory"
+    }
+    Get-Item -Path "$($DownloadFolder)/*.mp4" | ForEach-Object { $ExistVideos += $_.BaseName }
     $NeedVideos = $RankVideos | Where-Object { $ExistVideos -notcontains $_ }
 
-    $NeedVideos | ForEach-Object {
-        BiliDown $_
-    }
+    $NeedVideos | ForEach-Object { BiliDown $_ }
 }
-Main
+
+if (Test-Path $DownloadList) {
+    Main
+}
